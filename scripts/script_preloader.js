0b2e3c7427741ca2c80518fd44ab1019484e9566
Rodrigo <rodrigo@holbox.bz>
2013-05-23 10:32:47 -0500

    sync2

--- scripts/script_preloader.js
+++ scripts/script_preloader.js
@@ -1,6 +1,7 @@
-var loaderAnimation = $("#html5Loader").LoaderAnimation();
+/*var loaderAnimation = $("#html5Loader").LoaderAnimation();
 
 $.html5Loader({
 		getFilesToLoadJSON:'preload/files.json',
 		onUpdate: loaderAnimation.update
 });
+*/
