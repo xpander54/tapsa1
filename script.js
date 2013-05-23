7cfc7c4b06c3bab296b6b575b51ad093c8036a0d
Rodrigo <rodrigo@holbox.bz>
2013-05-23 11:59:30 -0500

    sync

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 console.log("doc ready");
 
 $('.carousel').carousel({
-				  interval: 3000
+				  interval: 3000,
+				  pause: "false"
 				})
 
 
@@ -16,8 +17,8 @@ function startParallax()
 {
 
 	/*$('.wrapper').localScroll(800);
-	
-	//.parallax(xPosition, speedFactor, outerHeight) options:
+
+		//.parallax(xPosition, speedFactor, outerHeight) options:
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
